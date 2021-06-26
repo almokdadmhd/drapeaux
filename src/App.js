53a6e6cc8639d58407897af9d9245c267a8ec2b5
rayen hidri <rayen.hidri@gmail.com>
2021-06-26 20:10:20 +0200

    installation de react + router

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Home from './pages/Home';
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter , Switch, Route} from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/a-propos" exact component={About} />
+        <Route exact component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+ 
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>A propos</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente illum suscipit, accusamus blanditiis obcaecati voluptatibus. Asperiores veniam dolorem ad, alias facere mollitia a eveniet inventore eaque odit ratione est magnam labore, dignissimos ab ea saepe doloribus libero consectetur eius, dolore delectus nostrum explicabo. In, necessitatibus. Officia expedita ut iusto harum omnis ipsum facere amet placeat optio earum. Atque dicta ex, voluptatum vel quaerat vitae doloribus necessitatibus ad laboriosam unde omnis, fuga soluta hic dolore perspiciatis consequatur debitis saepe eveniet deleniti consectetur itaque. Dicta harum consectetur ad, labore assumenda consequuntur natus dolorum nemo odio id officia blanditiis totam aperiam magnam corrupti excepturi mollitia ullam in impedit architecto, tempora ducimus autem. Possimus odio laborum nostrum fugiat atque optio eligendi ab explicabo iusto.</p>
+            <br/>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, dolore autem! Omnis in repellendus consectetur ue esse. Minus voluptatibus, dolorum blanditiis voluptatem fugit expedita hic enim veniam harum? Et, reiciendis error. Iste numquam eum error! Fugiat dolore asperiores alias aspernatur itaque a autem sapiente eius, dolor tenetur! Qui suscipit ipsa molestias esse distinctio. Deleniti, dolores, architecto molestias perferendis vero ratione ullam nostrum voluptate perspiciatis voluptatibus possimus? Voluptatum aut in neque deserunt, facere officia ipsum quae odit repellat non modi hic vel temporibus pariatur blanditiis iusto debitis sequi quod architecto ex magnam consequatur perspiciatis labore minima! Harum ipsum soluta temporibus perferendis illum quia? Delectus hic </p>
+
+        </div>
+     );
+}
+ 
+export default About;
