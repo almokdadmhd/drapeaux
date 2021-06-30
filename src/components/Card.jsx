fc3fc271cefe91518f69d3baed1569e28dafef4f
rayen hidri <rayen.hidri@gmail.com>
2021-06-30 04:05:52 +0200

    les props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
 
-const Card = () => {
-    return ( 
-        <div>Country</div>
-     );
+const Card = (props) => {
+    const { country } = props;
+
+    const NumberFormat = (x) => {
+      return  x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") ;
+    }
+     
+    return (
+      <li className="card">
+        <img src={country.flag} alt="falg" />
+        <div className="data-container">
+          <ul>
+            <li>{country.name}</li>
+            <li>{country.capital}</li>
+            <li>pop : {NumberFormat(country.population)}</li>
+          </ul>
+        </div>
+      </li>
+    );
 }
  
 export default Card;
