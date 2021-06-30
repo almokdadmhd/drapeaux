09dadabd03e5a1a441dd59da3cfbba006f80a11e
Rayen HIDRI <rayen.hidri@gmail.com>
2021-07-01 01:32:20 +0200

    tri dynamique

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -7,9 +7,12 @@ const Countries = () => {
   const [data, setData] = useState([]);
   const [sortedData, setSortedData] = useState([]);
   const [playOnce, setPlayOnce] = useState(true);
+  const [rangeValue, setRangeValue] = useState(40);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
   useEffect(() => {
-    if(playOnce){
+    if (playOnce) {
       axios
         .get(
           "https://www.restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
@@ -26,16 +29,48 @@ const Countries = () => {
       const sortedArray = countryObject.sort((a, b) => {
         return b.population - a.population;
       });
-      sortedArray.length=30;
+      sortedArray.length = rangeValue;
       setSortedData(sortedArray);
     };
     sortedCountry();
-  }, [data]);
+  }, [data, playOnce, rangeValue]);
 
   return (
     <div className="countries">
+      <div className="sort-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          value={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        <ul>
+          {radios.map((radio) => {
+            return (
+              <li>
+                <input
+                  type="radio"
+                  value={radio}
+                  id={radio}
+                  checked={radio === selectedRadio}
+                  onChange={(e) => setSelectedRadio(e.target.value)}
+                />
+                <label htmlFor={radio}>{radio}</label>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="cancel">
+          {selectedRadio && <h5 onClick={() =>setSelectedRadio('')}>Réinitialiser</h5>}
+      </div>
+
       <ul className="countries-list">
-        {sortedData.map((country) => (
+        {sortedData
+        .filter(((country) =>country.region.includes(selectedRadio)))
+        .map((country) => (
           <Card country={country} key={country.name} />
         ))}
       </ul>
